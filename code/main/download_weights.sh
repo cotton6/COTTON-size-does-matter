f6519360ab343f4b2c40837069ae5c46f3944451
culiver <andrew880111@gmail.com>
2023-03-04 20:21:54 +0800

    update url

--- code/main/download_weights.sh
+++ code/main/download_weights.sh
@@ -2,14 +2,14 @@
 FILE=result/Top_1024x768_COTTON/weights
 if ! test -d "$FILE"; then
     echo "$FILE not exists."
-    wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1-2RuuPLfrJ_Uia_BKrk2Pj58BSdWofgc' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1-2RuuPLfrJ_Uia_BKrk2Pj58BSdWofgc" -O result/Top_1024x768_COTTON/weights.zip && rm -rf /tmp/cookies.txt
+    wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=19_tXE7VSoT_z2JRZXAKuObM8T_w39WLS' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=19_tXE7VSoT_z2JRZXAKuObM8T_w39WLS" -O result/Top_1024x768_COTTON/weights.zip && rm -rf /tmp/cookies.txt
     unzip result/Top_1024x768_COTTON/weights.zip -d result/Top_1024x768_COTTON/
 fi
 
 FILE=result/Top_1024x768_DressCode/weights
 if ! test -d "$FILE"; then
     echo "$FILE not exists."
-    wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1eLSNbixRyVSa_EmZgCJ0niOS5V66F2U1' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1eLSNbixRyVSa_EmZgCJ0niOS5V66F2U1" -O result/Top_1024x768_DressCode/weights.zip && rm -rf /tmp/cookies.txt
+    wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1HPg4Qv3vorp7D9Cd4rn2inKx8J-nQJI4' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1HPg4Qv3vorp7D9Cd4rn2inKx8J-nQJI4" -O result/Top_1024x768_DressCode/weights.zip && rm -rf /tmp/cookies.txt
     unzip result/Top_1024x768_DressCode/weights.zip -d result/Top_1024x768_DressCode/
 fi
 
